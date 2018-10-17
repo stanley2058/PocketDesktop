b08561f545f23e632ff6872086dc38b692ea379d
stanley2058 <stanley2058@yahoo.com.tw>
2018-10-17 12:52:50 +0800

    加入專案檔案。

---
+++ PocketDesktop/MainDesktopWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace PocketDesktop
+{
+    /// <summary>
+    /// MainDesktopWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainDesktopWindow : Window
+    {
+        public MainDesktopWindow()
+        {
+            InitializeComponent();
+
+
+            var bitmap = (Bitmap)Properties.Resources.ResourceManager.GetObject("magnify");
+            if (bitmap == null) return;
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                SearchIcon.Source = bitmapImage;
+            }
+        }
+    }
+}
---
+++ PocketDesktop/PocketDesktop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace PocketDesktop
+{
+    public static class PocketDesktop
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var currentDir = Environment.CurrentDirectory;
+            if (!Directory.Exists(currentDir + "/PocketDesktop"))
+                Directory.CreateDirectory(currentDir + "/PocketDesktop");
+
+            new System.Windows.Application().Run(new MainDesktopWindow());
+        }
+    }
+}
